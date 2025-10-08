dba30907be2dd156ed08d78084755e9b05b80778
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-10-08 03:50:23 -0400

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/src/app/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/src/app/icon0.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/src/app/icon1.png" />
+        <link rel="apple-touch-icon" href="/src/app/apple-icon.png" />
+        <link rel="manifest" href="/src/app/manifest.json" />
+        <meta name="apple-mobile-web-app-title" content="Lapse" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
