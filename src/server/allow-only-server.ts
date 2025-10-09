3c88e0176e946844caacb3fcdb0809110dd7abdf
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-10-09 15:52:34 -0400

    refactor: also notify user if server-side code is included in client

--- src/server/allow-only-server.ts
+++ src/server/allow-only-server.ts
@@ -4,8 +4,11 @@ export function serverSideOnly() {
     if ("process" in globalThis)
         return;
     
-    console.trace("Server-side code has been imported by the client! This is VERY bad - please report at https://github.com/hackclub/lapse!");
-    throw new Error("Server-side code has been imported by the client! Please report this at https://github.com/hackclub/lapse!");
+    const err = "Server-side code has been imported by the client! This is VERY bad - please report at https://github.com/hackclub/lapse!";
+
+    console.trace(err);
+    alert(err);
+    throw new Error(err);
 }
 
 serverSideOnly();
