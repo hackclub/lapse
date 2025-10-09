b6238e2ad543449cd5612473bccd56d05183fbee
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-10-09 15:50:05 -0400

    feat: slight homepage layout adjustment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useAuth } from "../client/hooks/useAuth";
 import RootLayout from "@/client/components/RootLayout";
 import { Button } from "@/client/components/ui/Button";
+import LapseIcon from "../client/assets/icon.svg";
 import { LoadingModal } from "@/client/components/ui/LoadingModal";
 import Icon from "@hackclub/icons";
 
@@ -25,55 +26,56 @@ export default function Home() {
       <div className="flex w-full h-full py-8 items-center justify-center">
         <div className="max-w-4xl mx-auto px-6">
           <div className="text-center">
-            {
-              <div className="flex flex-col gap-8">
-                <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-8">
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col items-center justify-center gap-8">
+                  <LapseIcon className="w-32" />
                   <h1 className="text-6xl font-bold text-smoke leading-tight">
                     Lapse <sup>α</sup>
                   </h1>
-                  
-                  <p className="text-smoke text-xl leading-relaxed">
-                    {
-                      currentUser
-                        ? <>Thank you for participating in the alpha, <span className="text-cyan">{currentUser.displayName}</span>!</>
-                        : <>Lapse is a timelapse recording tool, currently in alpha.</>
-                    }
-                  </p>
                 </div>
                 
-                <div className="flex gap-4 justify-center">
+                <p className="text-smoke text-xl leading-relaxed">
                   {
                     currentUser
-                      ? (
-                        <>
-                          <Link href="/timelapse/create">
-                            <Button className="gap-2" kind="primary" onClick={() => {}}>
-                              <Icon glyph="plus-fill" size={24} />
-                              Create Timelapse
-                            </Button>
-                          </Link>
-                          
-                          <Button className="gap-2" onClick={signOut} kind="secondary">
-                            <Icon glyph="door-leave" size={24} />
-                            Sign Out
-                          </Button>
-                        </>
-                      )
-                      : (
-                        <Link href="/auth">
-                          <Button className="gap-2 px-16" kind="primary" onClick={() => {}}>
-                            <Icon glyph="welcome" size={24} />
-                            Sign in
+                      ? <>Thank you for participating in the alpha, <span className="text-cyan">{currentUser.displayName}</span>!</>
+                      : <>Lapse is a timelapse recording tool, currently in alpha.</>
+                  }
+                </p>
+              </div>
+              
+              <div className="flex gap-4 justify-center">
+                {
+                  currentUser
+                    ? (
+                      <>
+                        <Link href="/timelapse/create">
+                          <Button className="gap-2" kind="primary" onClick={() => {}}>
+                            <Icon glyph="plus-fill" size={24} />
+                            Create Timelapse
                           </Button>
                         </Link>
-                      )
+                        
+                        <Button className="gap-2" onClick={signOut} kind="secondary">
+                          <Icon glyph="door-leave" size={24} />
+                          Sign Out
+                        </Button>
+                      </>
+                    )
+                    : (
+                      <Link href="/auth">
+                        <Button className="gap-2 px-16" kind="primary" onClick={() => {}}>
+                          <Icon glyph="welcome" size={24} />
+                          Sign in
+                        </Button>
+                      </Link>
+                    )
 
-                  }
+                }
 
 
-                </div>
               </div>
-            }
+            </div>
           </div>
         </div>
       </div>
