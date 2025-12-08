c1d37424ce72755ad62db244ff52da7b5c9aea3e
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-12-08 13:05:04 -0500

    feat: add script to create user in database for testing

---
+++ apps/web/prisma/create-user.mjs
@@ -0,0 +1,49 @@
+// @ts-check
+"use strict";
+
+import { parseArgs } from "node:util";
+
+import { PrismaClient } from "../src/generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+async function main() {
+    const args = parseArgs({
+        options: {
+            email: { type: "string", default: "ascpixi@proton.me" },
+            sid: { type: "string", default: "U082DPCGPST" },
+            handle: { type: "string", default: "ascpixi" },
+            name: { type: "string", default: "ascpixi" },
+            pfp: { type: "string", default: "https://ca.slack-edge.com/T0266FRGM-U082DPCGPST-0c4754eb6211-512" }
+        }
+    });
+
+    console.log("");
+
+    const user = await prisma.user.create({
+        data: {
+            email: args.values.email ?? "ascpixi@proton.me",
+            slackId: args.values.sid,
+            handle: args.values.handle ?? "ascpixi",
+            displayName: args.values.name ?? "ascpixi",
+            profilePictureUrl: args.values.pfp,
+            bio: "",
+            urls: [],
+            permissionLevel: "USER",
+            createdAt: new Date(),
+            hackatimeApiKey: null,
+        },
+    });
+
+    console.log(`(info) User @${user.handle} created.`);
+    console.dir(user);
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
