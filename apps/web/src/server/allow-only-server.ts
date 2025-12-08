ac321dac7a11475fccf8da533a9fb222278b2fdb
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-12-08 13:04:31 -0500

    refactor: simplify server-side only check

--- apps/web/src/server/allow-only-server.ts
+++ apps/web/src/server/allow-only-server.ts
@@ -2,11 +2,11 @@ export function serverSideOnly() {
     if ("process" in globalThis)
         return;
     
-    const err = "Server-side code has been imported by the client! This is VERY bad - please report at https://github.com/hackclub/lapse!";
+    const apiErr = "Server-side code has been imported by the client! This is VERY bad - please report at https://github.com/hackclub/lapse!";
 
-    console.trace(err);
-    alert(err);
-    throw new Error(err);
+    console.trace(apiErr);
+    alert(apiErr);
+    throw new Error(apiErr);
 }
 
 serverSideOnly();
