42c52186bf6faca88befe8a78d725a800544e2ef
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-12-18 14:52:45 +0000

    fix: update Prisma config to v7

--- apps/web/src/server/db.ts
+++ apps/web/src/server/db.ts
@@ -1,14 +1,19 @@
 import "@/server/allow-only-server";
 
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma";
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
 export const database =
     globalForPrisma.prisma ??
-    new PrismaClient();
+    new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production")
     globalForPrisma.prisma = database;
