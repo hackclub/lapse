158dd1153fc7b09e8e211cd080270392e148a1b0
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-12-28 21:10:40 -0500

    fix(ui): make all modals scrollable (#76)
    * fix: make modals scrollable
    
    * fix(ui): fix scrolling in "Create timelapse" modal

--- apps/web/src/client/components/ui/Modal.tsx
+++ apps/web/src/client/components/ui/Modal.tsx
@@ -18,7 +18,7 @@ export function Modal({
 
   return (
     <div role="dialog" className={clsx(
-      "flex fixed w-screen h-screen p-4 sm:p-8 top-0 left-0 text-text bg-[#00000088] duration-500 transition-colors justify-center items-center shadow z-20",
+      "flex fixed w-screen h-screen p-4 sm:p-8 top-0 left-0 text-text bg-[#00000088] duration-500 transition-colors justify-center items-center shadow z-20 overflow-y-auto",
       !isOpen && "hidden",
       className
     )}>
