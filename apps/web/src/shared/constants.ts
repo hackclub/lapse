97f3395585c3e3d38c70ef933c568d1321dc0186
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-01-16 13:29:52 +0000

    fix: use proper file size limit in Formidable

--- apps/web/src/shared/constants.ts
+++ apps/web/src/shared/constants.ts
@@ -2,7 +2,6 @@
 // Aiming for ~6hrs of real-time footage to result in a timelapse that's ~10min long.
 export const TIMELAPSE_FRAME_LENGTH_MS = 2.5 * 1000;
 export const TIMELAPSE_FPS = 24;
-export const MAX_VIDEO_STREAM_SIZE = 200 * 1024 * 1024;
 export const MAX_VIDEO_FRAME_COUNT = 86400;
 export const THUMBNAIL_SIZE = 640;
 
