0a3dccc4c853b8c1c55a7aad8a907e445689a603
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-03-05 15:35:35 -0500

    feat: better indentation on console logs in worker

--- apps/worker/src/app.ts
+++ apps/worker/src/app.ts
@@ -4,11 +4,11 @@ import { realizeJobWorker } from "@/workers/realize.js";
 
 async function main() {
     console.log("⧗  Lapse (Background Worker) v2.0.0");
-    console.log(`   * started on ${new Date().toUTCString()}`);
+    console.log(`*  started on ${new Date().toUTCString()}`);
 
     await realizeJobWorker.run();
 
-    console.log("   * all workers running!");
+    console.log("*  all workers running!");
 }
 
 main();
