773770515957c225698e986bcaf2470be6a121f5
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-03-06 21:12:21 -0500

    fix: delete draft timelapses in IV migration

--- apps/server/prisma/migrations/20260306200917_add_iv/migration.sql
+++ apps/server/prisma/migrations/20260306200917_add_iv/migration.sql
@@ -4,5 +4,8 @@
   - Added the required column `iv` to the `DraftTimelapse` table without a default value. This is not possible if the table is not empty.
 
 */
+-- Delete all existing draft timelapses
+DELETE FROM "DraftTimelapse";
+
 -- AlterTable
 ALTER TABLE "DraftTimelapse" ADD COLUMN     "iv" TEXT NOT NULL;
