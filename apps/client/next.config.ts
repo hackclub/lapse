12bda8e37f9ca19065995e50755eda259b0aaa35
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-03-10 07:18:01 -0400

    fix: remove Sentry from `client`

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,4 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 import type { Configuration } from "webpack";
 import TerserPlugin from "terser-webpack-plugin";
@@ -78,37 +77,5 @@ let config: NextConfig = {
     ];
   },
 };
-
-// If we have Sentry environment variables set up, then use Sentry.
-if (process.env.SENTRY_ORG && process.env.SENTRY_PROJECT) {
-  config = withSentryConfig(config, {
-    // For all available options, see:
-    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
-
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: true,
-
-    webpack: {
-      treeshake: {
-        removeDebugLogging: false
-      }
-    }
-  });
-}
 
 export default config;
