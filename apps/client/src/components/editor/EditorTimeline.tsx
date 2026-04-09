d2c5a815202f0d8cafbb8978d7abdb69a9dab065
Lamparter <71598437+Lamparter@users.noreply.github.com>
2026-04-09 12:29:39 +0100

    Allow deleting with the actual DEL key

--- apps/client/src/components/editor/EditorTimeline.tsx
+++ apps/client/src/components/editor/EditorTimeline.tsx
@@ -43,7 +43,7 @@ export function EditorTimeline({ sessions, editList, setEditList, playback, onSa
 
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
-      if (event.key == "Backspace") {
+      if (event.key == "Backspace" || "Delete") {
         const target = event.target as HTMLElement
         if (["INPUT", "TEXTAREA"].includes(target?.tagName) == false) {
           setEditList(editList.filter((_, i) => i !== selectedEditRegionIdx));
