258e91fb61dcb0408644dce039e7e76d1a2fc0c8
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-06-01 10:58:07 -0400

    fix: revert devscripts sleep to local definition

--- apps/devscripts/src/common/util.ts
+++ apps/devscripts/src/common/util.ts
@@ -1,1 +1,4 @@
-export { sleep } from "@hackclub/lapse-shared";
+/**
+ * Returns a `Promise<void>` that resolves after `ms` milliseconds.
+ */
+export const sleep = (ms: number) => new Promise<void>(resolve => setTimeout(resolve, ms));
