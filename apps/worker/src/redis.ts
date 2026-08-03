1e6f6a59311a90351babeaec90f8deb65d9817d5
Echo <github@3kh0.net>
2026-08-03 11:29:03 -0400

    chore: update deps (#257)

--- apps/worker/src/redis.ts
+++ apps/worker/src/redis.ts
@@ -2,4 +2,8 @@ import { Redis } from "ioredis";
 
 import { env } from "@/env.js";
 
-export const redis = new Redis(env.REDIS_URL, { maxRetriesPerRequest: null });
+export const redis = new Redis(env.REDIS_URL, {
+    maxRetriesPerRequest: null,
+    // ioredis 6 defaults to RESP3; keep RESP2 until Redis/BullMQ soak tests pass
+    protocol: 2,
+});
